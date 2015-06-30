797747573573968d45725c5662f738a91b72015c
mauriciomartinez <mandarino12345678@gmail.com>
2015-06-30 11:35:37 +0200

    adpatacion bd heroku

--- models/models.js
+++ models/models.js
@@ -1,24 +1,43 @@
 var path = require('path');
-
+//adptacion heroku nube
+//Postgres DATABASE_URL = postgres://user:passwd@host:pot/DATABASE_URL
+//SQLite DATABASE_URL = sqlite://:@:/
+//postgres://qvuhcdegbcbcyd:MMfyzJbjbiF96IUrbXVxsJeEl7@ec2-54-227-249-165.compute-1.amazonaws.com:5432/d74p7v2ppr0trn
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name  = (url[6]||null);
+var user     = (url[2]||null);
+var pwd      = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect  = (url[1]||null);
+var port     = (url[5]||null);
+var host     = (url[4]||null);
+var storage  = process.env.DATABASE_STORAGE;
 //carga modelo ORM
 var Sequelize =  require('sequelize');
 
 //Usar BBDD SQLite
-var sequelize = new Sequelize(null,null,null,
-                  {dialect:'sqlite',storage: 'quiz.sqlite'}
-    );
+var sequelize = new Sequelize(DB_name, user, pwd,
+       { dialect:  protocol,
 
+    protocol: protocol,
+    port:     port,
+    host:     host,
+    storage:  storage,  // solo SQLite (.env)
+    omitNull: true      // solo Postgres
+  }
+);
+ var quiz_path = path.join(__dirname,'quiz');
 //importar la Definicion
-var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+var Quiz = sequelize.import(quiz_path);
 exports.Quiz = Quiz;
 //sequelize.sync crea e inicializa tabla de preguntas
-sequelize.sync().success(function(){
-  Quiz.count().success(function (count){
+sequelize.sync().then(function(){
+  Quiz.count().then(function (count){
       if(count === 0){//la tabal se incializa si esta vacia
           Quiz.create({
                         pregunta: 'Capital Italia',
                         respuesta: 'Roma'
-                      }).success(function(){console.log('Base de datos iniclizada');});
+                      }).then(function(){console.log('Base de datos iniclizada');});
       }
   });
 });
